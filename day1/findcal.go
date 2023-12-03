fb7b6b62c04b393ac75183641eee78d6b5edecf1
SenorGatos <yourgitemail@example.com>
2023-12-03 09:03:30 -0500

    part2 attempt, words being added but still not getting correct sum

--- day1/findcal.go
+++ day1/findcal.go
@@ -5,13 +5,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
+
+var numberWords = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var mapWords = make(map[int]int)
 
 // check if character is a number
 func isNumber(char byte) bool {
 	return char >= '0' && char <= '9'
 }
 
+//function to identify whole word numbers
+
+func findNumberWord(word string) {
+	ind := 0
+	for i, number := range numberWords {
+		ind = strings.Index(word, number)
+		if ind == -1 {
+			continue
+		}
+		// fmt.Println(word, "  ", "Found", number, "at index", ind, "as", i+1)
+		mapWords[ind] = i + 1
+	}
+}
+
+func findNumber(word string) {
+	numbersInLine := []int{}
+	for i := 0; i < len(word); i++ {
+		if isNumber(word[i]) {
+			numbersInLine = append(numbersInLine, int(word[i])-48)
+			// fmt.Println(int(word[i])-48, " ", word[i])
+			mapWords[i] = int(word[i]) - 48
+		}
+	}
+}
+
+func sort_map(m map[int]int) {
+
+	keys := make([]int, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println(k, ":", m[k])
+	}
+}
 func main() {
 	sum := 0
 	// read file input
@@ -28,23 +69,36 @@ func main() {
 		line := scanner.Text()
 		// Process each line of the file here
 
-		//declare slice
-		numbersInLine := []int{}
+		fmt.Println("Working on line: ", line)
+		// find words and numbers
+		findNumberWord(line)
+		findNumber(line)
 
-		for i := 0; i < len(line); i++ {
-			if isNumber(line[i]) {
-				numbersInLine = append(numbersInLine, int(line[i])-48)
-				fmt.Println(int(line[i])-48, " ", line[i])
+		if len(mapWords) == 1 {
+			fmt.Println("Adding ", mapWords[0]*10+mapWords[0])
+			sum = sum + mapWords[0]*10 + mapWords[0]
+			continue
+		}
+		sort_map(mapWords)
+		maxKey := 0
+		minKey := 1000
+		for k := range mapWords {
+			if k > maxKey {
+				maxKey = k
+			}
+			if k < minKey {
+				minKey = k
 			}
 		}
-		fmt.Println(numbersInLine)
-		if len(numbersInLine) == 0 {
-			fmt.Println("No numbers in line")
-		} else if len(numbersInLine) == 1 {
-			sum = sum + numbersInLine[0]*10 + numbersInLine[0]
-		} else {
-			sum = sum + numbersInLine[0]*10 + numbersInLine[len(numbersInLine)-1]
+		fmt.Println("First word: ", mapWords[minKey], " Last word: ", mapWords[maxKey])
+		fmt.Println("Adding ", mapWords[minKey]*10+mapWords[maxKey])
+		sum = sum + mapWords[minKey]*10 + mapWords[maxKey]
+
+		// clear the map every time
+		for k := range mapWords {
+			delete(mapWords, k)
 		}
+
 	}
 
 	fmt.Println(sum)
